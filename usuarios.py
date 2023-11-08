c67e04520298b0f2ca11c3ab5e981619729308ba
Letticia08 <149123382+Letticia08@users.noreply.github.com>
2023-11-07 21:37:27 -0300

    gerenciamento de usuario

--- usuarios.py
+++ usuarios.py
@@ -6,4 +6,4 @@ usuarios_blueprint = Blueprint('usuarios', __name__)
 
 @usuarios_blueprint.route("/usuarios")
 def usuarios():
-  return render_template("dashboard.html")
+  return render_template("usuario.html")
