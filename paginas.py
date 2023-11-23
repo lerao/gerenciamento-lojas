0f00cce2d8c9c6608d6cc8a14a33336889c039b6
Henrique Bernardo <144061167+henriquebernardoc@users.noreply.github.com>
2023-11-23 16:14:23 -0300

    Update paginas.py

--- paginas.py
+++ paginas.py
@@ -1,32 +1,33 @@
 
-from flask import Flask, render_template, request, redirect, Blueprint
-
-paginas_blueprint = Blueprint('paginas', __name__)
-
-@paginas_blueprint.route("/paginas")
-def paginas():
-  return render_template("paginas.html")
-
-
-
 from flask import Flask, render_template, request, redirect, Blueprint, url_for
 import requests
-from flask_cors import CORS
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Pagina.db'  # Configuração para usar SQLite
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Evita avisos desnecessários
+db = SQLAlchemy(app)
 
 paginas_blueprint = Blueprint('paginas', __name__)
 
 @paginas_blueprint.route("/paginas")
 def paginas():
   return render_template("paginas.html")
+
+paginas_blueprint = Blueprint('paginas', __name__)
+@paginas_blueprint.route("/paginas")
+def paginas():
 
   backend_url = 'http://127.0.0.1:5000/paginas'
   response = requests.get(backend_url)
+
   if response.status_code == 200:
-        categorias = response.json()
-        return render_template('categorias.html', dados=categorias, categoria=(0,0))
+      paginas = response.json()
+      return render_template('paginas.html', dados=paginas, pagina=(0,0))
   else:
-    return render_template("categorias.html", dados=[], categoria=(0,0))
-  
+    return render_template("paginas.html", dados=[], pagina=(0,0))
+ 
 @paginas_blueprint.route("/paginas/delete/<id>")
 def paginaDelete(id):
   bd = db.SQLiteConnection('Pagina.db')
